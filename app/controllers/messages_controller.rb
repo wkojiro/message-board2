977bb11787dc19ffee0ddb7f560887284dd27b10
wkojiro1 <wkojiro@eazydrive.co.jp>
2016-04-07 07:56:51 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,19 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new #Messageモデルのオブジェクトの初期化
+    @messages = Message.all #すべて取り出す。
   end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path, notice: 'メッセージを保存しました。'
+   # redirect_to 'http://www.google.co.jp/'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name,:body)
+  end
+  ##　ここまで
 end
