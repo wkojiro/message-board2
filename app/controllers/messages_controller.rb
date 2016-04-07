504534d0573276788b69dc9356313bfb95760edc
wkojiro1 <wkojiro@eazydrive.co.jp>
2016-04-07 08:58:59 +0000

    View not work

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,14 +6,20 @@ class MessagesController < ApplicationController
   
   def create
     @message = Message.new(message_params)
-    @message.save
+    if @message.save
     redirect_to root_path, notice: 'メッセージを保存しました。'
    # redirect_to 'http://www.google.co.jp/'
+    else
+   #メッセージが保存できなかった場合
+     @message = Message.all
+     flash.now[:alert] ="メッセージの保存に失敗しました。"
+     render 'index'
+    end
   end
   
   private
   def message_params
     params.require(:message).permit(:name,:body)
   end
-  ##　ここまで
+  ##ここまで
 end
