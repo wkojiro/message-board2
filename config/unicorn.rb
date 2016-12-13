12250c25815f75b7b614ced56b69cb5e27a68018
wkojiro1 <wkojiro@eazydrive.co.jp>
2016-12-13 07:25:44 +0000

    deploy test5

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -28,13 +28,14 @@ before_fork do |server, worker|
   old_pid = "#{server.config[:pid]}.oldbin"
     if old_pid != server.pid
       begin
-        sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-        Process.kill(sig, File.read(old_pid).to_i)
+        # sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+        # Process.kill(sig, File.read(old_pid).to_i)
+         Process.kill "QUIT", File.read(old_pid).to_i  
       rescue Errno::ENOENT, Errno::ESRCH
       end
     end
 
-    sleep 1
+    # sleep 1
   end
 
 after_fork do |server, worker|
